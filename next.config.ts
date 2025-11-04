5e2e1ef992306dfbf74f19284ffc7cd5065d771f
Jeffrey Entekume <entekumejeffrey@gmail.com>
2025-11-04 09:05:50 +0100

    refer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Exclude test files from webpack bundling
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    
+    // Ignore test directories in production
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        './test/data/05-versions-space.pdf': 'commonjs ./test/data/05-versions-space.pdf'
+      });
+    }
+    
+    return config;
+  },
+  
+  // Exclude test files from static file serving
+  async rewrites() {
+    return [];
+  },
+  
+  // Configure file system access
+  experimental: {
+    serverComponentsExternalPackages: ['pdf-parse'],
+  },
 };
 
 export default nextConfig;
